7b08f550dbe2fd087f7c628fba20dd96624309f3
charleshuy <pathofdoomz@gmail.com>
2024-05-09 01:04:10 +0700

    First version

---
+++ src/main/java/org/huycharles/mathutil/MathUtilMaven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.huycharles.mathutil;
+
+/**
+ *
+ * @author patho
+ */
+public class MathUtilMaven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/org/huycharles/mathutil/core/MathUtilTest.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package org.huycharles.mathutil.core;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author patho
+ */
+public class MathUtilTest {
+    
+    @Test 
+    public void testFactorialGivenRightArgumentReturnsWell(){
+        assertEquals(6789, 6789);
+    }
+    
+}
